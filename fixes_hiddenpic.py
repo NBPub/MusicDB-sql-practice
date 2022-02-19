5c39f60f43b5a79e9d50319ca9fa6c1a2fbae75f
NBPub <80983660+NBPub@users.noreply.github.com>
2022-02-18 18:04:42 -0800

    Update fixes_hiddenpic.py
    added comments, reformated 2 parter

--- fixes_hiddenpic.py
+++ fixes_hiddenpic.py
@@ -13,14 +13,15 @@ import stat
 
 # Another two parter
 
+# Pass 1, cover art exists in folder
 # Scans and keeps 1 image
 # From hidden > 0, folder images NOT NULL
 
-con = sqlite3.connect('five.db')
+con = sqlite3.connect('database.db')
 total = con.execute("SELECT COUNT(path) FROM Albums WHERE hidden IS NOT NULL AND images IS NOT NULL").fetchone()[0]
 query = "SELECT artist, album_name, path FROM Albums WHERE hidden IS NOT NULL AND images IS NOT NULL"
-pics = ['.jpg','.jpeg','.gif','.png','.tif', '.tiff', '.webp', '.bmp']
-names = ['cover','folder','front']
+pics = ['.jpg','.jpeg','.gif','.png','.tif', '.tiff', '.webp', '.bmp'] # image formats
+names = ['cover','folder','front'] # image names accepted by music player
 
 renamed = 0
 unhidden = 0
@@ -31,7 +32,7 @@ pbar = tqdm(con.execute(query), total=total, unit='♥')
 for art, alb, path in pbar:
     pbar.set_postfix({art:alb})
     
-    # Gather images
+    # Gather all images, including hidden
     art = []
     for file in Path(path).iterdir():
         if file.suffix in pics:
@@ -45,6 +46,57 @@ for art, alb, path in pbar:
         deleted += 1
     check = ordered[0]["path"]
 
+    # Rename leftover image, if needed
+    if check.stem.lower() not in names:
+        check = check.rename(Path(path,f'folder{check.suffix}'))
+        renamed += 1
+    
+    # Unhide image, if hidden
+    if bool(check.stat().st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN):
+        win32api.SetFileAttributes(str(check),win32con.FILE_ATTRIBUTE_NORMAL)
+        unhidden += 1
+        
+print('\n\n', 'deleted:',deleted, 'renamed:', renamed, 'unhidden:', unhidden, '\n\n', total)
+con.close()
+     
+
+        
+        
+# Pass 2, no cover art in folder       
+# Scans and keeps 1 image
+# From hidden > 0, folder images NULL
+
+con = sqlite3.connect('database.db') # name db
+total = con.execute("SELECT COUNT(path) FROM Albums WHERE hidden IS NOT NULL AND images IS NULL").fetchone()[0]
+query = "SELECT artist, album_name, path, hidden FROM Albums WHERE hidden IS NOT NULL AND images IS NULL"
+pics = ['.jpg','.jpeg','.gif','.png','.tif', '.tiff', '.webp', '.bmp']
+names = ['cover','folder','front']
+
+renamed = 0
+unhidden = 0
+deleted = 0
+
+pbar = tqdm(con.execute(query), total=total, unit='♥')
+
+for art, alb, path, hidden in pbar:
+    pbar.set_postfix({art:alb})
+    
+    # Gather images (hidden only with this query)
+    art = []
+    for file in Path(path).iterdir():
+        if file.suffix in pics:
+            d = {"path": file, "size": file.stat().st_size}
+            art.append(d)
+    
+    # If more than one image, keep only biggest file
+    if hidden > 1:
+        ordered = sorted(art, key = lambda x: x['size'], reverse=True) # sort images by descending file size
+        for file in ordered[1:]:
+            file["path"].unlink()
+            deleted += 1
+        check = ordered[0]["path"]
+    else:
+        check = art[0]["path"]
         
     
     # Rename leftover image, if needed
@@ -52,78 +104,10 @@ for art, alb, path in pbar:
         check = check.rename(Path(path,f'folder{check.suffix}'))
         renamed += 1
     
+    # Unhide, if statement shouldn't be necessary
     if bool(check.stat().st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN):
         win32api.SetFileAttributes(str(check),win32con.FILE_ATTRIBUTE_NORMAL)
         unhidden += 1
 
-
-
-        
 print('\n\n', 'deleted:',deleted, 'renamed:', renamed, 'unhidden:', unhidden, '\n\n', total)
-con.close()
-     
-
-
-# art = []
-# for file in p.iterdir():
-#     if file.suffix in pics:
-#         art.append(file)
-
-# art = []
-# for file in p.iterdir():
-#     if file.suffix in pics:
-#         d = {"path": file, "size": file.stat().st_size, "name": file.stem}
-#         art.append(d)
-        
-        
-        
-# Scans and keeps 1 image
-# From hidden > 0, folder images NULL
-
-# con = sqlite3.connect('five.db')
-# total = con.execute("SELECT COUNT(path) FROM Albums WHERE hidden IS NOT NULL AND images IS NULL").fetchone()[0]
-# query = "SELECT artist, album_name, path, hidden FROM Albums WHERE hidden IS NOT NULL AND images IS NULL"
-# pics = ['.jpg','.jpeg','.gif','.png','.tif', '.tiff', '.webp', '.bmp']
-# names = ['cover','folder','front']
-
-# renamed = 0
-# unhidden = 0
-# deleted = 0
-
-# pbar = tqdm(con.execute(query), total=total, unit='♥')
-
-# for art, alb, path, hidden in pbar:
-#     pbar.set_postfix({art:alb})
-    
-#     # Gather images (hidden only with this query)
-#     art = []
-#     for file in Path(path).iterdir():
-#         if file.suffix in pics:
-#             d = {"path": file, "size": file.stat().st_size}
-#             art.append(d)
-    
-#     # If more than one image, keep only biggest file
-#     if hidden > 1:
-#         ordered = sorted(art, key = lambda x: x['size'], reverse=True) # sort images by descending file size
-#         for file in ordered[1:]:
-#             file["path"].unlink()
-#             deleted += 1
-#         check = ordered[0]["path"]
-#     else:
-#         check = art[0]["path"]
-        
-    
-#     # Rename leftover image, if needed
-#     if check.stem.lower() not in names:
-#         check = check.rename(Path(path,f'folder{check.suffix}'))
-#         renamed += 1
-    
-#     if bool(check.stat().st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN):
-#         win32api.SetFileAttributes(str(check),win32con.FILE_ATTRIBUTE_NORMAL)
-#         unhidden += 1
-
-
-
-        
-# print('\n\n', 'deleted:',deleted, 'renamed:', renamed, 'unhidden:', unhidden, '\n\n', total)
-# con.close()        
+con.close()        
